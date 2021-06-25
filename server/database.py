01b4ecff6192889c1cb85df096fb25e6a34e8c42
Adrian Alan Pol <adrianalan.pol@gmail.com>
2021-06-25 14:28:38 +0200

    Add SET API

--- server/database.py
+++ server/database.py
@@ -9,3 +9,22 @@ class GPUDatabase():
     def connect(self, url):
         self.client = pymongo.MongoClient(url)
         self.db = self.client["gpustat"]
+
+    def set_state(self, planet, gpu, users, time):
+        col = self.db[planet]
+        new_status = {
+            "_id": gpu,
+            "users": users,
+            "time": time
+        }
+        query = {
+            "_id": gpu
+        }
+        update = {
+            "$set": new_status
+        }
+        if [*col.find(query)]:
+            col.update_one(query, update)
+        else:
+            col.insert_one(new_status)
+        return True
--- server/mpp-gpumon.py
+++ server/mpp-gpumon.py
@@ -11,5 +11,46 @@ app = Flask(__name__, template_folder="templates")
 config = yaml.safe_load(open('server/mpp-gpumon.yml'))
 db = GPUDatabase(url=config['database']['url'])
 
+
+def set_state(planet, gid, timestamp, users):
+    return db.set_state(planet, gid, users, timestamp)
+
+
+def verify(key, magic, vcode, timestamp):
+    t = time.time()
+    # Filter requests older than 10s
+    if (t - int(timestamp) > 10):
+        return False
+    # Decode the message
+    c = ('echo -n "{}" | base64 -d |'
+         'openssl rsautl -decrypt -inkey {} -in -').format(vcode, key)
+    out = subprocess.check_output(c, shell=True)
+    with open(magic) as f:
+        s = f.read()
+    # Verify authenticity
+    return out.decode().rstrip() == str(timestamp) + s
+
+
+@app.route('/api/set', methods=['POST'])
+def set_api():
+    content = request.get_json()
+    planet = content['planet']
+    gid = content['gid']
+    users = content['usr']
+    timestamp = content['t']
+    vcode = content['v']
+    key = config['key']
+    magic = config['magic']
+    if verify(key, vcode, timestamp):
+        try:
+            set_state(planet, gid, timestamp, users)
+            r = jsonify(success=True)
+        except Exception as e:
+            r = jsonify(success=False)
+    else:
+        r = jsonify(success=False)
+    return r
+
+
 if __name__ == '__main__':
     app.run()
